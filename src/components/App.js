2c418cd939fdce1ef3c376c7e8783bf04ba39123
jurgob <jurgo.boemo@gmail.com>
2016-06-07 00:33:28 +0100

    fetch list feeds

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React from 'react'
+const {array, shape, string} = React.PropTypes
+const jsonize = (res) => res.json();
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      redditRes: {}
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://www.reddit.com/r/all.json")
+      .then(jsonize)
+      .then( json =>  this.setState({redditRes: json}))
+  }
+
+  render(){
+      const {redditRes} = this.state
+
+      return redditRes.data
+        ? <FeedList feeds={redditRes.data.children} />
+        : <LoadingFeeds />
+
+
+  }
+
+}
+
+const LoadingFeeds = () => <div>Loading Feeds ... </div>
+
+
+const FeedList = ({feeds}) => <div>{feeds.map( (feed, idx) => <Feed key={idx}  feed={feed} /> ) }</div>
+FeedList.propTypes = {
+  feeds: array.isRequired
+}
+
+
+const Feed = ({feed}) => {
+  const {data} = feed;
+  const {title} = data;
+
+  return (
+    <div>
+      <h3>{title}</h3>
+    </div>
+  )
+}
+Feed.propTypes = {
+  data: shape({
+    title: string
+  })
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App'
+
 // import { Provider } from 'react-redux';
-// import { Router, browserHistory } from 'react-router'
-// import routers from './routers';
 // import store from './store';
 
 
 ReactDOM.render(
-    (<div>React test</div>),
+    (<App />),
     document.getElementById('root')
 );
