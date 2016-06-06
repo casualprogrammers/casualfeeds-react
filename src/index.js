192fdcf5767f667fa20347058e9ca4c4f95e6a15
jurgob <jurgo.boemo@gmail.com>
2016-06-06 23:44:00 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { Router, browserHistory } from 'react-router'
+// import routers from './routers';
+// import store from './store';
+
+
+ReactDOM.render(
+    (<div>React test</div>),
+    document.getElementById('root')
+);
